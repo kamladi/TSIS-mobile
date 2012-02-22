a50defdfafe1f67caf7fae4f62c38cf4d092988d
Kedar Amladi <kamladi@gmail.com>
2012-02-22 17:42:10 -0500

    replaced $.ajax method with shortcut $.getJSON
    also used $.map to customize data array

--- app.js
+++ app.js
@@ -4,40 +4,38 @@ TODO:
 		SoundCloud's new HTML5 audio player
 */
 $(document).ready(function() {
-	var original_link = 'http://thissongissick.com/blog/feed/';
-	var yql_link = "http://query.yahooapis.com/v1/public/yql"+
-		"?q=select%20*%20from%20rss%20where%20url%3D'http%3A%2F%2Fthissongissick.com%2Fblog%2Ffeed%2F'"+
-		"&format=json&diagnostics=true&callback=jsonCallback";
-	console.log("requesting rss feed: \n" + yql_link);
-	$.ajax({
-		url: yql_link,
-		success: function() {
-			console.log('data successfully retrieved');
-		}
-	});
-	
-	function jsonCallback(data) {
-		console.log('hello');
-		var count = data.query.count;
-		$(data.query.results.item).each(function(index) {
-			var title = this.title; 
-				//console.log('title: '+title);
-				var titles = title.split(":");
-			    var maintitle = titles[0];
-			    var subtitle = titles[1];
-			var link = this.link; 
-				//console.log('link: '+link);
-			var description = this.description; 
-				//console.log('description: '+description);
-			var content = this.encoded;
-			
-			//build homepage listview
+	var url = 'http://thissongissick.com/blog/feed/';
+	var yql_request = "select * from rss where url='" + url + "'";
+	var yql_link = 'http://query.yahooapis.com/v1/public/yql?q='+
+		encodeURIComponent(yql_request)+
+		'&format=json&callback=?';
+	console.log("requesting rss feed: \n" + url);
+	var posts;
+	$.getJSON(yql_link, function(data) {
+		console.log('json data retrieved:');
+		console.log(data);
+		posts = $.map(data.query.results.item, function(post) {
+			var titles = post.title.split(':');
+			return {
+				maintitle: titles[0],
+				subtitle: titles[1],
+				link: post.link,
+				description: post.description,
+				imgsrc: $(post.encoded).find('img').first().attr('src'),
+				content: post.encoded,
+				tags: post.category 
+			};
+		});
+		console.log('posts array mapped:');
+		console.log(posts);
+		$.each(posts, function(index, post) {
+			//build homepage list item
 			$('#list').append(
 				'<li>' + 
-					//'<img style="float: left; max-width: 80px; max-height: 80px; margin: 5px;" src="'+$(content).find('img').first().attr('src')+'" />' +
+					'<img style="float: left; max-width: 80px; max-height: 80px; margin: 5px;" src="'+post.imgsrc+'" />' +
 					'<a href=\"#post-'+index+'\">' +
-						'<h3 style="white-space: normal !important;">' + maintitle + '</h3>' + 
-						'<p style="white-space: normal !important;">' + subtitle + '</p>' + 
+						'<h3 style="white-space: normal !important;">' + post.maintitle + '</h3>' + 
+						'<p style="white-space: normal !important;">' + post.subtitle + '</p>' + 
 					'</a>' + 
 					'<div style="clear: left;"></div>' + 
 				'</li>'
@@ -48,16 +46,16 @@ $(document).ready(function() {
 			'<div data-role="page" data-theme="b" id="post-'+index+'" data-url="post-'+index+'">' + 
 				'<header data-role="header">' +
 				    '<a href="#" data-rel="back" data-role="button" data-icon="arrow-l">Back</a>' +
-					'<h3 style="white-space: normal !important;">'+maintitle+'</h3>' +
+					'<h3 style="white-space: normal !important;">'+post.maintitle+'</h3>' +
 				'</header>' + 
 				'<div data-role="content">'+
-					'<p>'+content+'</p>' + 
+					'<p>'+post.content+'</p>' + 
 				'</div>' +
 			'</div>'	
 			);
 		});
-		
-		//refresh homepage
-		$('#home').page();
-	}
+
+		//refresh home page
+		$('#list').listview('refresh');
+	});
 });
